6ad608c128cba933c7c46906d257ea817edb9d6d
isaacs <i@izs.me>
2011-09-02 17:30:21 -0700

    require('fs') lies about lstat support

--- rimraf.js
+++ rimraf.js
@@ -11,7 +11,7 @@ try {
   fs = require("fs")
 }
 
-var lstat = fs.lstat ? "lstat" : "stat"
+var lstat = process.platform === "win32" ? "stat" : "lstat"
   , lstatSync = lstat + "Sync"
 
 // for EMFILE handling
