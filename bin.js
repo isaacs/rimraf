3d533f664a96fe2c071717c39473f9065d0e9bf7
isaacs <i@izs.me>
2013-06-21 07:50:38 -0700

    bin: Support multiple args
    Also, add -- as a flag to mean 'no more cli args', so you can
    remove a file named '--help' for example.

--- bin.js
+++ bin.js
@@ -2,14 +2,32 @@
 
 var rimraf = require('./')
 
-if (process.argv.length === 3 && process.argv[2] && process.argv[2] != '--help' && !/^\-[a-z]*h[a-z]*$/.test(process.argv[2]) && process.argv[2] != 'help' && process.argv[2] != '/?') {
-  rimraf.sync(process.argv[2])
+var help = false
+var dashdash = false
+var args = process.argv.slice(2).filter(function(arg) {
+  if (dashdash)
+    return !!arg
+  else if (arg === '--')
+    dashdash = true
+  else if (arg.match(/^(-+|\/)(h(elp)?|\?)$/))
+    help = true
+  else
+    return !!arg
+});
+
+if (help || args.length === 0) {
+  // If they didn't ask for help, then this is not a "success"
+  var log = help ? console.log : console.error
+  log('Usage: rimraf <path>')
+  log('')
+  log('  Deletes all files and folders at "path" recursively.')
+  log('')
+  log('Options:')
+  log('')
+  log('  -h, --help    Display this usage info')
+  process.exit(help ? 0 : 1)
 } else {
-  console.log('Usage: rimraf <path>')
-  console.log()
-  console.log('  Deletes all files and folders at "path" recursively.')
-  console.log()
-  console.log('Options:')
-  console.log()
-  console.log('  -h, --help    Display this usage info')
+  args.forEach(function(arg) {
+    rimraf.sync(arg)
+  })
 }
