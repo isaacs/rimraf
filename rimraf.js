dd25b8bb650b62e17437142c72d03aae28bbe879
Casey Webb <notcaseywebb@gmail.com>
2016-11-04 18:21:08 -0400

    Add Promise Support (Drop Node v0.10)

--- rimraf.js
+++ rimraf.js
@@ -41,14 +41,15 @@ function defaults (options) {
 }
 
 function rimraf (p, options, cb) {
-  if (typeof options === 'function') {
-    cb = options
-    options = {}
+  switch (typeof options) {
+    case 'function':
+      cb = options
+    case 'undefined':
+      options = {}
   }
 
   assert(p, 'rimraf: missing path')
   assert.equal(typeof p, 'string', 'rimraf: path should be a string')
-  assert.equal(typeof cb, 'function', 'rimraf: callback function required')
   assert(options, 'rimraf: invalid options argument provided')
   assert.equal(typeof options, 'object', 'rimraf: options should be object')
 
@@ -58,59 +59,71 @@ function rimraf (p, options, cb) {
   var errState = null
   var n = 0
 
-  if (options.disableGlob || !glob.hasMagic(p))
-    return afterGlob(null, [p])
-
-  options.lstat(p, function (er, stat) {
-    if (!er)
+  return new Promise(function (resolve, reject) {
+    if (options.disableGlob || !glob.hasMagic(p))
       return afterGlob(null, [p])
 
-    glob(p, options.glob, afterGlob)
-  })
+    options.lstat(p, function (er, stat) {
+      if (!er)
+        return afterGlob(null, [p])
 
-  function next (er) {
-    errState = errState || er
-    if (--n === 0)
-      cb(errState)
-  }
-
-  function afterGlob (er, results) {
-    if (er)
-      return cb(er)
-
-    n = results.length
-    if (n === 0)
-      return cb()
-
-    results.forEach(function (p) {
-      rimraf_(p, options, function CB (er) {
-        if (er) {
-          if (isWindows && (er.code === "EBUSY" || er.code === "ENOTEMPTY" || er.code === "EPERM") &&
-              busyTries < options.maxBusyTries) {
-            busyTries ++
-            var time = busyTries * 100
-            // try again, with the same exact callback as this one.
-            return setTimeout(function () {
-              rimraf_(p, options, CB)
-            }, time)
-          }
-
-          // this one won't happen if graceful-fs is used.
-          if (er.code === "EMFILE" && timeout < options.emfileWait) {
-            return setTimeout(function () {
-              rimraf_(p, options, CB)
-            }, timeout ++)
-          }
-
-          // already gone
-          if (er.code === "ENOENT") er = null
-        }
-
-        timeout = 0
-        next(er)
-      })
+      glob(p, options.glob, afterGlob)
     })
-  }
+
+    function next (er) {
+      errState = errState || er
+      if (--n === 0)
+        errState
+          ? reject(errState)
+          : resolve()
+    }
+
+    function afterGlob (er, results) {
+      if (er)
+        return reject(er)
+
+      n = results.length
+      if (n === 0)
+        return resolve()
+
+      results.forEach(function (p) {
+        rimraf_(p, options, function CB (er) {
+          if (er) {
+            if (isWindows && (er.code === "EBUSY" || er.code === "ENOTEMPTY" || er.code === "EPERM") &&
+                busyTries < options.maxBusyTries) {
+              busyTries ++
+              var time = busyTries * 100
+              // try again, with the same exact callback as this one.
+              return setTimeout(function () {
+                rimraf_(p, options, CB)
+              }, time)
+            }
+
+            // this one won't happen if graceful-fs is used.
+            if (er.code === "EMFILE" && timeout < options.emfileWait) {
+              return setTimeout(function () {
+                rimraf_(p, options, CB)
+              }, timeout ++)
+            }
+
+            // already gone
+            if (er.code === "ENOENT") er = null
+          }
+
+          timeout = 0
+          next(er)
+        })
+      })
+    }
+  })
+    .then(function () {
+      if (cb) cb()
+    })
+    .catch(function (er) {
+      return cb
+        ? cb(er)
+        : Promise.reject(er)
+    })
 }
 
 // Two possible strategies.
