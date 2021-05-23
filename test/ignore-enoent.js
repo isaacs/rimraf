2d033772b376348c7ff93aee9c1ef348ac3e7261
isaacs <i@izs.me>
2021-05-23 16:41:55 -0700

    ignore ENOENT errors on Windows as well

---
+++ test/ignore-enoent.js
@@ -0,0 +1,19 @@
+const t = require('tap')
+const { ignoreENOENT, ignoreENOENTSync } = require('../lib/ignore-enoent.js')
+
+const enoent = Object.assign(new Error('no ent'), { code: 'ENOENT' })
+const eperm = Object.assign(new Error('eperm'), { code: 'EPERM' })
+
+const throwEnoent = () => {
+  throw enoent
+}
+const throwEperm = () => {
+  throw eperm
+}
+
+t.resolves(ignoreENOENT(Promise.reject(enoent)), 'enoent is fine')
+t.rejects(ignoreENOENT(Promise.reject(eperm)), { code: 'EPERM' },
+  'eperm is not')
+t.doesNotThrow(() => ignoreENOENTSync(throwEnoent), 'enoent is fine sync')
+t.throws(() => ignoreENOENTSync(throwEperm), { code: 'EPERM' },
+  'eperm is not fine sync')
