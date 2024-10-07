8ef2df141088f38639d45475f99f6b84dd75e5c2
Luke Karrys <luke@lukekarrys.com>
2024-10-07 09:54:17 -0700

    revert use native

--- src/use-native.ts
+++ src/use-native.ts
@@ -8,14 +8,11 @@ const hasNative = major > 14 || (major === 14 && minor >= 14)
 
 // we do NOT use native by default on Windows, because Node's native
 // rm implementation is less advanced.  Change this code if that changes.
-const doNotUseNative =
-  !hasNative || process.platform === 'win32' ? () => false : null
-
-// signal and filter options are not available on native methods
-const hasNativeOptions = (opt?: RimrafAsyncOptions | RimrafOptions) =>
-  !opt?.signal && !opt?.filter
-
-export const useNative =
-  doNotUseNative ?? ((o?: RimrafAsyncOptions) => hasNativeOptions(o))
-export const useNativeSync =
-  doNotUseNative ?? ((o?: RimrafOptions) => hasNativeOptions(o))
+export const useNative: (opt?: RimrafAsyncOptions) => boolean =
+  !hasNative || process.platform === 'win32' ?
+    () => false
+  : opt => !opt?.signal && !opt?.filter
+export const useNativeSync: (opt?: RimrafOptions) => boolean =
+  !hasNative || process.platform === 'win32' ?
+    () => false
+  : opt => !opt?.signal && !opt?.filter
