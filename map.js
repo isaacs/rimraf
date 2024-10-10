549d1e8bf5a68b0d31478246ef0145d87f8a1e8e
Luke Karrys <luke@lukekarrys.com>
2024-10-10 10:59:11 -0700

    Configure tap coverage-map

--- map.js
+++ map.js
@@ -1,1 +1,7 @@
-module.exports = test => test.replace(/^test/, 'lib')
+export default test =>
+  test.startsWith('/test/integration/') ? null : (
+    (test.endsWith('/bin.ts') ? test.replace(/\.ts$/, '.mts') : test).replace(
+      /^test/,
+      'src',
+    )
+  )
